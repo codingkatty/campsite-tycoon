7447ce90c18aaca7340d574607d1d1a0fb1ed9cc
candy <candyisakat@gmail.com>
2026-01-13 22:06:30 +0800

    camp build + basic building gui

--- scripts/build_cursor.gd
+++ scripts/build_cursor.gd
@@ -1,6 +1,9 @@
-extends Sprite2D
+extends Node2D
 
 @onready var tilemap = get_parent().get_node("map/TileMap")
+@onready var pointer1 = get_node("1t")
+@onready var pointer2_h = get_node("2t_horizontal")
+@onready var pointer2_v = get_node("2t_vertical")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +13,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	var tilepos = tilemap.local_to_map(mouse_position)
-	position = tilepos * 16
+	pointer2_h.position = tilepos * 16
 
-	if Input.is_action_pressed("build"):
-		tilemap.set_cell(2, tilepos, -1, Vector2i(1, 6))
-		tilemap.set_cells_terrain_connect(2, [tilepos], 0, 1, false)
+	if Input.is_action_just_pressed("build"):
+		print("build")
+		tilemap.set_cell(2, tilepos, 0, Vector2i(14, 5))
+		#tilemap.set_cells_terrain_connect(2, [tilepos], 0, 1, false)
